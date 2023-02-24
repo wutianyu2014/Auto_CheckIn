5621aa991a861c309248a1bf134aefa7c4db8fc5
wutianyu <wutianyu@croot.com>
2023-02-24 15:15:56 +0800

    glados支持批量签到，优化代码

--- glados_checkin.py
+++ glados_checkin.py
@@ -11,7 +11,8 @@ from push_message import *
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-def __get_header(cookie_header):
+
+def _get_header(cookie_header):
     header = {
         'Connection': 'close',
         'Accept': '*/*',
@@ -29,45 +30,47 @@ def __get_header(cookie_header):
     }
     return header
 
+
 # 解决出现警告 Adding certificate verification is strongly advised.
 urllib3.disable_warnings()
 
+# 分隔符
 split_str = '&@@&'
 
-# def glados_checkIn(glados_cookie):
-#     checkin_message = []
-#     # 判断是否设置
-#     if glados_cookie is None or len(glados_cookie) <= 0:
-#         print("The glados_cookie is none")
-#         return checkin_message
-#
-#     glados_cookie = glados_cookie.split(split_str)
-#     # 遍历cookie执行签到，并返回签到状态码和签到信息
-#     for idx, cookie in enumerate(glados_cookie):
-#         time.sleep(10)
-#         print(f"[glados_Account_{idx + 1}]:")
-#         header = __get_header(cookie)
-#         result = checkIn(header)
-#         # 存在账户签到信息，说明成功执行了签到
-#         checkin_message.append(f">### glados_Account_{idx + 1} checkin message\n" + str(result) + "\n - - - \n")
-#     return checkin_message
-#
-# # 单条签到
-# def checkIn(header):
-#     checkin_message = []
-#     # 1、签到
-#     result_json = _check_in(header)
-#
-#     code = result_json["code"]
-#     message = result_json["message"]
-#     usr_status = getStatus(header)
-#
-#     checkin_message.append("**【签到状态码】**  " + str(code) + " <br>")
-#     checkin_message.append("**【签到信息】**  " + str(message) + " <br>")
-#     checkin_message.append("**【email】**  " + str(_get(usr_status, 'email')) + " <br>")
-#     checkin_message.append("**【剩余天数】**  " + str(_get(usr_status, 'days')) + " <br>")
-#
-#     return ''.join(checkin_message)
+def batch_checkIn(glados_cookie):
+    result_list = []
+    # 判断是否设置
+    if glados_cookie is None or len(glados_cookie) <= 0:
+        print("The glados_cookie is none")
+        return result_list
+
+    glados_cookie = glados_cookie.split(split_str)
+    # 遍历cookie执行签到，并返回签到状态码和签到信息
+    for idx, cookie in enumerate(glados_cookie):
+        time.sleep(10)
+        print(f"[glados_Account_{idx + 1}]:")
+        header = _get_header(cookie)
+        result = checkIn(header)
+        # 存在账户签到信息，说明成功执行了签到
+        result_list.append(result)
+    return result_list
+
+# 单条签到
+def checkIn(header):
+    # 1、签到
+    result_json = _check_in(header)
+
+    code = result_json["code"]
+    message = result_json["message"]
+    usr_status = getStatus(header)
+
+    data = {
+        'code': str(code),
+        'message': str(message),
+        'email': str(_get(usr_status, 'email')),
+        'leftDays': str(_get(usr_status, 'leftDays'))
+    }
+    return data
 
 # 签到
 def _check_in(header):
@@ -79,7 +82,7 @@ def _check_in(header):
         # 1、签到
         resp = requests.post(url='https://glados.rocks/api/user/checkin', headers=header, json=data)
         result_json = resp.json()
-        print('[_check_in response]', str(result_json))
+        print('[_check_in response]', result_json['code'] ,result_json['message'])
         resp.close()
     except Exception:
         traceback.print_exc()
@@ -108,35 +111,37 @@ def getStatus(header):
         else:
             status_data = result_json["data"]
     except Exception:
-            traceback.print_exc()
-            print('getStatus fail')
+        traceback.print_exc()
+        print('getStatus fail')
     return status_data
+
+# 封装推送信息
+def _get_message(result_list):
+    title = []
+    message = []
+    # enumerate函数中通过start参数指定从1开始数数，遍历还是从下标0开始
+    for idx, data in enumerate(result_list, start=1):
+        title.append(f"{idx}A{data['code']};")
+
+        message.append(f">### glados_Account_{idx} checkin message\n")
+        message.append("**【签到状态码】**  " + data['code'] + " <br>")
+        message.append("**【签到信息】**  " + data['message'] + " <br>")
+        message.append("**【email】**  " + data['email'] + " <br>")
+        message.append("**【剩余天数】**  " + data['leftDays'] + " <br>")
+        message.append("\n - - - \n")
+    return ''.join(title), ''.join(message)
 
 if __name__ == '__main__':
     # glados平台cookie
-    juejin_cookie = os.environ['GLADOS_COOKIE']
+    glados_cookie = os.environ['GLADOS_COOKIE']
     # pushplus平台token
     pushplus_token = os.environ['PUSHPLUS_TOKEN']
 
-    header = __get_header(juejin_cookie)
-
-    checkin_message = []
-    # 1、签到
-    result_json = _check_in(header)
-
-    code = result_json["code"]
-    message = result_json["message"]
-    usr_status = getStatus(header)
-
-    checkin_message.append("**【签到状态码】**  " + str(code) + " <br>")
-    checkin_message.append("**【签到信息】**  " + str(message) + " <br>")
-    checkin_message.append("**【email】**  " + str(_get(usr_status, 'email')) + " <br>")
-    checkin_message.append("**【剩余天数】**  " + str(_get(usr_status, 'leftDays')) + " <br>")
-
-    send_message = ''.join(checkin_message)
+    result_list = batch_checkIn(glados_cookie)
+    title, send_message = _get_message(result_list)
 
     # 第一次推送
-    msg = pushplus_message(pushplus_token, message, send_message, None)
+    msg = pushplus_message(pushplus_token, title, send_message, None)
     if msg is not None:
         # 推送失败再次推送
         time.sleep(10)
